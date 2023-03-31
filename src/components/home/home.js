e96205ee115877155ae767619d52718f3c699194
KaYing CHU <chukayingyuki@gmail.com>
2023-03-31 10:36:13 +0100

    made some progress

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,52 +1,122 @@
 import React from "react";
-import ProgressBar from "./progress";
-import { Card } from "./card";
+import { Tile } from "./tile";
 import { Content } from "./content";
 import { Grid } from '@mui/material';
-import square from '../../images/square.png';
 import axios from 'axios';
+import ImageList from '@mui/material/ImageList';
+import Stack from '@mui/material/Stack';
 import { useState, useEffect } from 'react';
 
 const Home = () => {
 
 
-    const [card, setCard] = React.useState({});
+    const [movies, setMovies] = React.useState([
+        {
+            imdbID:"tt9850370",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850371",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850372",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850373",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850374",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850375",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850376",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850377",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        },
+        {
+            imdbID:"tt9850378",
+            title:"#AnneFrank. Parallel Stories",
+            posterURLs: {
+                original:"https://image.tmdb.org/t/p/original/hkC4yNDFmW1yQuQhtZydMeRuaAb.jpg"
+            }
+        }
+    ]);
 
 
 
 
     function getMovie(search_title, country_code) {
+        console.log("exec once")
             const options = {
-              method: 'GET',
-              url: 'https://streaming-availability.p.rapidapi.com/v2/search/title',
-              params: {title: 'batman', country: 'us', type: 'movie', output_language: 'en'},
-              headers: {
-                'X-RapidAPI-Key': '23e7de4453mshb034b7870fedc6bp18739fjsn4e8925f0ee59',
-                'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-              }
+                method: 'GET',
+                url: 'https://streaming-availability.p.rapidapi.com/search/basic',
+                params: {
+                  country: 'us',
+                  service: 'netflix',
+                  type: 'movie',
+                  genre: '878'
+                },
+                headers: {
+                    'X-RapidAPI-Key': '23e7de4453mshb034b7870fedc6bp18739fjsn4e8925f0ee59',
+                    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+                }
             };
 
             axios.request(options).then(function (response) {
-                const movie = response.data.result[0];
-                console.log("movie");
-                setCard({
-                    "title" : movie.title,
-                    "description" : movie.overview,
-                    "image" : movie.posterURLs['500']
-                });
+                setMovies(response.data.results);
+                console.log("movies");
             }).catch(function (error) {
                 console.error(error);
             });
     }
 
+    useEffect(() => {
+        // getMovie('batman','us')
+    }, []);
+
     return (
         <div className="home">
 
             <div className="movie">
-                <p>title : {card.title}</p>
-                <p>overview : {card.description}</p>
-                <p>{getMovie('batman','us')}</p>
-                
+                {/* <ImageList sx={{ overflowX: 'auto', width: 500, height: 340 }} > */}
+                    <Stack sx={{ overflowX: 'auto', width: '70vw', height: 340 }} direction="row" spacing={2}>
+                        {movies.map((movie)=>(<Tile movie={movie} />))}
+                    </Stack>
+                {/* </ImageList> */}
             </div>
 
         </div>
